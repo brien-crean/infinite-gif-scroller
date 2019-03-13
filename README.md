172cf092efb25f6ab15c11a2d41d2369ef33469d
Brien Crean <brien.crean@gmail.com>
2019-03-12 17:54:21 -0700

    Added a simple implementation of the intersection observer api to change the colour of a div depending on how much of the div is visible

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # intersection-observer-api experiments
 
-- Simple project to explore the features of the Javascript Intersection Observer API and it's features
+- Simple project to explore the features of the Javascript Intersection Observer API with ReactJS
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+//components
+import Image from './components/Image';
+import './components/Image.css';
 
 class App extends Component {
+
+  renderImages = () => {
+    let images = [];
+    for(let i=0; i<10; i++) {
+      images.push(<Image key={i} imageID={`image${i}`} />);
+    }
+    return images;
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Interaction Observer API Test</h1>
         </header>
+        <div className="App-images">
+        
+          { 
+            this.renderImages()
+          }
+        </div>
+
       </div>
     );
   }
