4d52135ceeef89ce00c598892f5e286f59e58f65
modus spare <modusspare@moduss-MacBook-Pro.local>
2019-04-15 21:26:33 -0700

    refactored List class component to a functional component and implemented useContext hook

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,39 +1,32 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react'
 import Video from '../Video/Video';
 import AppContext from '../../AppContext';
 import './List.css';
 
-class List extends Component {
-
-  renderVideos = (gifs) => {
-    let videos = [];
-    gifs.map(gif => {
-      videos.push(<Video key={gif.id} videoURL={gif.images.original_mp4.mp4} />);
-      return null;
-    });
-    return videos;
-  };
-
-  render() {
-    const { gifs } = this.context.state;
-    return (
-      <div className="List">
-        <header className="List-header">
-          <h1 className="List-title">Infinite Scroller using the Interaction Observer API</h1>
-        </header>
-        <div className="List-description">
-          <p>Javascript Interaction Observer API used control video element playback based on each elements visibility</p>
-          <p>The top 10 trending GIFs are pulled as mp4s from GIPHY</p>
-        </div>
-        <div className="List-videos">
-          {
-            gifs ? this.renderVideos(gifs) : null
-          }
-        </div>
-        <button className="Fetch-gifs" onClick={this.context.fetchGIFs}>More...</button>
+const List = () => {
+  const context = useContext(AppContext);
+  const { gifs } = context.state;
+  return (
+    <div className="List">
+      <header className="List-header">
+        <h1 className="List-title">Infinite Scroller using the Interaction Observer API</h1>
+      </header>
+      <div className="List-description">
+        <p>Javascript Interaction Observer API used control video element playback based on each elements visibility</p>
+        <p>The top 10 trending GIFs are pulled as mp4s from GIPHY</p>
       </div>
-    )
-  }
+      <div className="List-videos">
+        {
+          gifs ?
+            gifs.map(gif => (
+              <Video key={gif.id} videoURL={gif.images.original_mp4.mp4} />
+            ))
+            : null
+        }
+      </div>
+      <button className="Fetch-gifs" onClick={context.fetchGIFs}>More...</button>
+    </div>
+  )
 }
-List.contextType = AppContext;
+
 export default List;
